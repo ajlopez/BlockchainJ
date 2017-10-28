3cc16a8978d4fb0b1366ce54ac36b2d40190922a
ajlopez <webmaster@ajlopez.com>
2017-10-28 12:30:06 -0300

    Using sha3, first simple test

---
+++ src/test/java/com/ajlopez/blockchain/utils/HashUtilsTest.java
@@ -0,0 +1,19 @@
+package com.ajlopez.blockchain.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+/**
+ * Created by ajlopez on 28/10/2017.
+ */
+public class HashUtilsTest {
+    @Test
+    public void getSha3Hash() throws NoSuchProviderException, NoSuchAlgorithmException {
+        byte[] hash = HashUtils.sha3(new byte[] { 0x01, 0x02, 0x03 });
+
+        Assert.assertNotNull(hash);
+    }
+}
