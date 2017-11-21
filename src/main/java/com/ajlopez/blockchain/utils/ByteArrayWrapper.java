8e2b03a811f8fcce0014ee0a13618e14592114f0
ajlopez <webmaster@ajlopez.com>
2017-11-21 14:51:56 -0300

    ByteArrayWrapper, equals, hashCode

--- src/main/java/com/ajlopez/blockchain/utils/ByteArrayWrapper.java
+++ src/main/java/com/ajlopez/blockchain/utils/ByteArrayWrapper.java
@@ -1,5 +1,7 @@
 package com.ajlopez.blockchain.utils;
 
+import java.util.Arrays;
+
 /**
  * Created by ajlopez on 21/11/2017.
  */
@@ -12,5 +14,18 @@ public class ByteArrayWrapper {
 
     public byte[] getBytes() {
         return this.bytes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this.bytes);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ByteArrayWrapper))
+            return false;
+
+        return Arrays.equals(this.bytes, ((ByteArrayWrapper)obj).bytes);
     }
 }
--- src/test/java/com/ajlopez/blockchain/utils/ByteArrayWrapperTest.java
+++ src/test/java/com/ajlopez/blockchain/utils/ByteArrayWrapperTest.java
@@ -15,4 +15,22 @@ public class ByteArrayWrapperTest {
 
         Assert.assertArrayEquals(data, wrapper.getBytes());
     }
+
+    @Test
+    public void createWithSameData() {
+        byte[] data1 = new byte[] { 0x01, 0x02, 0x03 };
+        byte[] data2 = new byte[] { 0x01, 0x02, 0x03 };
+
+        ByteArrayWrapper wrapper1 = new ByteArrayWrapper(data1);
+        ByteArrayWrapper wrapper2 = new ByteArrayWrapper(data2);
+
+        Assert.assertEquals(wrapper1, wrapper2);
+        Assert.assertEquals(wrapper1.hashCode(), wrapper2.hashCode());
+
+        Assert.assertTrue(wrapper1.equals(wrapper2));
+        Assert.assertTrue(wrapper2.equals(wrapper1));
+
+        Assert.assertFalse(wrapper1.equals(null));
+        Assert.assertFalse(wrapper1.equals("foo"));
+    }
 }
