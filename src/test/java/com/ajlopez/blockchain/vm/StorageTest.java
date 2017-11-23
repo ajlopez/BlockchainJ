de2d6b82bc145ae43d86c77770cbca2173660d38
ajlopez <webmaster@ajlopez.com>
2017-11-23 10:08:06 -0300

    Additional test for Storage

--- src/test/java/com/ajlopez/blockchain/vm/StorageTest.java
+++ src/test/java/com/ajlopez/blockchain/vm/StorageTest.java
@@ -15,7 +15,7 @@ public class StorageTest {
     }
 
     @Test
-    public void setAndGetUndefinedValue() {
+    public void setAndGetValue() {
         byte[] key = new byte[] { 0x01, 0x02, 0x03 };
         byte[] value = new byte[] { 0x04, 0x05 };
 
@@ -23,5 +23,20 @@ public class StorageTest {
 
         storage.setValue(key, value);
         Assert.assertArrayEquals(value, storage.getValue(key));
+    }
+
+    @Test
+    public void setAndGetValueUsingDifferentKeys() {
+        byte[] key = new byte[] { 0x01, 0x02, 0x03 };
+        byte[] value = new byte[] { 0x04, 0x05 };
+        byte[] key1 = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+        byte[] key2 = new byte[] { 0x00, 0x00, 0x01, 0x02, 0x03 };
+
+        Storage storage = new Storage();
+
+        storage.setValue(key, value);
+        Assert.assertArrayEquals(value, storage.getValue(key));
+        Assert.assertArrayEquals(value, storage.getValue(key1));
+        Assert.assertArrayEquals(value, storage.getValue(key2));
     }
 }
