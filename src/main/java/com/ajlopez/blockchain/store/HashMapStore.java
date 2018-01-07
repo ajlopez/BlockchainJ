4ccea13855a039a4d537f6a0683d56bb0192358a
ajlopez <webmaster@ajlopez.com>
2018-01-07 08:26:46 -0300

    Minor refactor

--- src/main/java/com/ajlopez/blockchain/store/HashMapStore.java
+++ src/main/java/com/ajlopez/blockchain/store/HashMapStore.java
@@ -3,12 +3,13 @@ package com.ajlopez.blockchain.store;
 import com.ajlopez.blockchain.utils.ByteArrayWrapper;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ajlopez on 06/01/2018.
  */
 public class HashMapStore implements KeyValueStore {
-    private HashMap<ByteArrayWrapper, byte[]> values = new HashMap<>();
+    private Map<ByteArrayWrapper, byte[]> values = new HashMap<>();
 
     public byte[] getValue(byte[] key) {
         return this.values.get(new ByteArrayWrapper(key));
