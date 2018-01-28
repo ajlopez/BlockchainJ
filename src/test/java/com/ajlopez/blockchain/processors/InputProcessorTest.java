d35fee865afd9b757391257ac945d93955c3891a
ajlopez <webmaster@ajlopez.com>
2018-01-28 12:40:05 -0300

    InputProcessor additional test

--- src/test/java/com/ajlopez/blockchain/processors/InputProcessorTest.java
+++ src/test/java/com/ajlopez/blockchain/processors/InputProcessorTest.java
@@ -41,4 +41,36 @@ public class InputProcessorTest {
         Assert.assertNotNull(result);
         Assert.assertEquals(block.getHash(), result.getHash());
     }
+
+    @Test
+    public void processTenRepeatedBlockMessage() throws InterruptedException {
+        BlockProcessor blockProcessor = FactoryHelper.createBlockProcessor();
+
+        Block block = new Block(0, null);
+        Message message = new BlockMessage(block);
+
+        MessageProcessor messageProcessor = new MessageProcessor(blockProcessor, null);
+
+        InputProcessor processor = new InputProcessor(messageProcessor);
+
+        Semaphore sem = new Semaphore(0, true);
+
+        processor.onEmpty(() -> {
+            sem.release();
+        });
+
+        for (int k = 0; k < 10; k++)
+            processor.postMessage(message);
+
+        processor.start();
+
+        sem.acquire();
+
+        processor.stop();
+
+        Block result = blockProcessor.getBestBlock();
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(block.getHash(), result.getHash());
+    }
 }
