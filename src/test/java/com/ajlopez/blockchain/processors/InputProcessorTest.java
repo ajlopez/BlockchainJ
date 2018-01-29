9d9bcdd39de78776c14ba35da5e45c8528e20d88
ajlopez <webmaster@ajlopez.com>
2018-01-29 11:16:32 -0300

    InputProcessor additional test

--- src/test/java/com/ajlopez/blockchain/processors/InputProcessorTest.java
+++ src/test/java/com/ajlopez/blockchain/processors/InputProcessorTest.java
@@ -1,12 +1,15 @@
 package com.ajlopez.blockchain.processors;
 
 import com.ajlopez.blockchain.core.Block;
+import com.ajlopez.blockchain.core.Transaction;
 import com.ajlopez.blockchain.messages.BlockMessage;
 import com.ajlopez.blockchain.messages.Message;
+import com.ajlopez.blockchain.messages.TransactionMessage;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -72,5 +75,41 @@ public class InputProcessorTest {
 
         Assert.assertNotNull(result);
         Assert.assertEquals(block.getHash(), result.getHash());
+    }
+
+    @Test
+    public void processTransactionMessage() throws InterruptedException {
+        TransactionPool pool = new TransactionPool();
+        TransactionProcessor transactionProcessor = new TransactionProcessor(pool);
+
+        Transaction transaction = FactoryHelper.createTransaction(100);
+        Message message = new TransactionMessage(transaction);
+
+        MessageProcessor messageProcessor = new MessageProcessor(null, transactionProcessor);
+
+        InputProcessor processor = new InputProcessor(messageProcessor);
+
+        Semaphore sem = new Semaphore(0, true);
+        processor.onEmpty(() -> {
+            sem.release();
+        });
+
+        processor.postMessage(message);
+        processor.start();
+
+        sem.acquire();
+
+        processor.stop();
+
+        List<Transaction> transactions = pool.getTransactions();
+
+        Assert.assertNotNull(transactions);
+        Assert.assertFalse(transactions.isEmpty());
+        Assert.assertEquals(1, transactions.size());
+
+        Transaction result = transactions.get(0);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(transaction.getHash(), result.getHash());
     }
 }
