21ea070f36edda6f9f8dcee8ee9743ffe9f8520a
ajlopez <webmaster@ajlopez.com>
2018-02-05 11:49:05 -0300

    Status message

---
+++ src/test/java/com/ajlopez/blockchain/net/messages/StatusMessageTest.java
@@ -0,0 +1,22 @@
+package com.ajlopez.blockchain.net.messages;
+
+import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.utils.HashUtils;
+import com.ajlopez.blockchain.utils.HashUtilsTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ajlopez on 05/02/2018.
+ */
+public class StatusMessageTest {
+    @Test
+    public void createWithData() {
+        Hash nodeid = HashUtilsTest.generateRandomHash();
+        StatusMessage message = new StatusMessage(nodeid, 2, 3);
+
+        Assert.assertEquals(nodeid, message.getNodeId());
+        Assert.assertEquals(2, message.getNetworkNumber());
+        Assert.assertEquals(3, message.getBestBlockNumber());
+    }
+}
