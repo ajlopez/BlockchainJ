062b5bb2131b0fb84e34b30b6ac529faf69607ed
ajlopez <webmaster@ajlopez.com>
2018-03-05 14:15:47 -0300

    Additional TrieStore test

--- src/test/java/com/ajlopez/blockchain/store/TrieStoreTest.java
+++ src/test/java/com/ajlopez/blockchain/store/TrieStoreTest.java
@@ -5,10 +5,14 @@ import com.ajlopez.blockchain.utils.HashUtilsTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
 /**
  * Created by ajlopez on 04/03/2018.
  */
 public class TrieStoreTest {
+    private static Random random = new Random();
+
     @Test
     public void retrieveNoTrie() {
         TrieStore store = new TrieStore(new HashMapStore());
@@ -20,6 +24,24 @@ public class TrieStoreTest {
     public void saveAndRetrieveEmptyTrie() {
         TrieStore store = new TrieStore(new HashMapStore());
         Trie trie = Trie.getEmptyTrie();
+
+        store.save(trie);
+
+        Trie result = store.retrieve(trie.getHash());
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(trie.getHash(), result.getHash());
+    }
+
+    @Test
+    public void saveAndRetrieveTrieWithKeyValue() {
+        byte[] value = new byte[32];
+        random.nextBytes(value);
+        byte[] key = new byte[2];
+
+        TrieStore store = new TrieStore(new HashMapStore());
+
+        Trie trie = Trie.getEmptyTrie().put(key, value);
 
         store.save(trie);
 
