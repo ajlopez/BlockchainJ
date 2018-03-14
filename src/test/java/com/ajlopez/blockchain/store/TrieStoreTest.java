b89ee9e262346b34ed206e1c374c2a2beba63d07
ajlopez <webmaster@ajlopez.com>
2018-03-14 14:33:43 -0300

    Testing trie multiple keys, WIP

--- src/test/java/com/ajlopez/blockchain/store/TrieStoreTest.java
+++ src/test/java/com/ajlopez/blockchain/store/TrieStoreTest.java
@@ -55,4 +55,35 @@ public class TrieStoreTest {
         Assert.assertNotNull(rvalue);
         Assert.assertArrayEquals(rvalue, value);
     }
+
+    @Test
+    public void saveAndRetrieveTrieWithOneHundredKeyValues() {
+        byte[][] values = new byte[100][];
+        byte[][] keys = new byte[100][];
+
+        for (int k = 0; k < 100; k++) {
+            keys[k] = new byte[1];
+            keys[k][0] = (byte)k;
+
+            values[k] = new byte[32];
+            random.nextBytes(values[k]);
+        }
+
+        TrieStore store = new TrieStore(new HashMapStore());
+
+        Trie trie = new Trie(store);
+
+        for (int k = 0; k < 100; k++)
+            trie = trie.put(keys[k], values[k]);
+
+        trie.save();
+
+        Trie result = store.retrieve(trie.getHash());
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(trie.getHash(), result.getHash());
+
+        for (int k = 0; k < 100; k++)
+            Assert.assertArrayEquals(values[k], result.get(keys[k]));
+    }
 }
