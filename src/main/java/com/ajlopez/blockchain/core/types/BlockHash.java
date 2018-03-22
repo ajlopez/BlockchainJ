57045179566e0d36c44b07248b744fa84ccb9cc4
ajlopez <webmaster@ajlopez.com>
2018-03-22 17:46:36 -0300

    Adding BlockHash, WIP

---
+++ src/main/java/com/ajlopez/blockchain/core/types/BlockHash.java
@@ -0,0 +1,10 @@
+package com.ajlopez.blockchain.core.types;
+
+/**
+ * Created by ajlopez on 22/03/2018.
+ */
+public class BlockHash extends Hash {
+    public BlockHash(byte[] bytes) {
+        super(bytes);
+    }
+}
