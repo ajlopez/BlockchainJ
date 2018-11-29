a7b4eed7e387dbced459897c660507e3deb286f6
ajlopez <webmaster@ajlopez.com>
2018-11-29 10:49:39 -0300

    Generate genesis with initial accounts

--- src/main/java/com/ajlopez/blockchain/bc/GenesisGenerator.java
+++ src/main/java/com/ajlopez/blockchain/bc/GenesisGenerator.java
@@ -3,6 +3,7 @@ package com.ajlopez.blockchain.bc;
 import com.ajlopez.blockchain.core.Block;
 import com.ajlopez.blockchain.core.types.BlockHash;
 import com.ajlopez.blockchain.state.Trie;
+import com.ajlopez.blockchain.store.AccountStore;
 
 /**
  * Created by ajlopez on 29/11/2018.
@@ -13,6 +14,10 @@ public class GenesisGenerator {
     }
 
     public static Block generateGenesis() {
-        return new Block(0, BlockHash.EMPTY_BLOCK_HASH, Trie.EMPTY_TRIE_HASH);
+        return generateGenesis(new AccountStore(new Trie()));
+    }
+
+    public static Block generateGenesis(AccountStore accountStore) {
+        return new Block(0, BlockHash.EMPTY_BLOCK_HASH, accountStore.getRootHash());
     }
 }
--- src/test/java/com/ajlopez/blockchain/bc/GenesisGeneratorTest.java
+++ src/test/java/com/ajlopez/blockchain/bc/GenesisGeneratorTest.java
@@ -1,10 +1,15 @@
 package com.ajlopez.blockchain.bc;
 
+import com.ajlopez.blockchain.core.Account;
 import com.ajlopez.blockchain.core.Block;
 import com.ajlopez.blockchain.core.types.BlockHash;
 import com.ajlopez.blockchain.state.Trie;
+import com.ajlopez.blockchain.store.AccountStore;
+import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigInteger;
 
 /**
  * Created by ajlopez on 29/11/2018.
@@ -20,6 +25,24 @@ public class GenesisGeneratorTest {
         Assert.assertEquals(BlockHash.EMPTY_BLOCK_HASH, genesis.getParentHash());
         Assert.assertNotNull(genesis.getStateRootHash());
         Assert.assertEquals(Trie.EMPTY_TRIE_HASH, genesis.getStateRootHash());
+        Assert.assertNotNull(genesis.getTransactions());
+        Assert.assertTrue(genesis.getTransactions().isEmpty());
+    }
+
+    @Test
+    public void generateGenesisWithInitialAccounts() {
+        AccountStore accountStore = new AccountStore(new Trie());
+
+        accountStore.putAccount(FactoryHelper.createRandomAddress(), new Account(BigInteger.TEN, 42));
+
+        Block genesis = GenesisGenerator.generateGenesis(accountStore);
+
+        Assert.assertNotNull(genesis);
+        Assert.assertEquals(0, genesis.getNumber());
+        Assert.assertNotNull(genesis.getParentHash());
+        Assert.assertEquals(BlockHash.EMPTY_BLOCK_HASH, genesis.getParentHash());
+        Assert.assertNotNull(genesis.getStateRootHash());
+        Assert.assertEquals(accountStore.getRootHash(), genesis.getStateRootHash());
         Assert.assertNotNull(genesis.getTransactions());
         Assert.assertTrue(genesis.getTransactions().isEmpty());
     }
