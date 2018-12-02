709b42e5cbc33ff6d593beba7565d9c50aaeaf35
ajlopez <webmaster@ajlopez.com>
2018-12-02 10:30:44 -0300

    BlocksProvider

---
+++ src/main/java/com/ajlopez/blockchain/jsonrpc/BlocksProvider.java
@@ -0,0 +1,38 @@
+package com.ajlopez.blockchain.jsonrpc;
+
+import com.ajlopez.blockchain.bc.BlockChain;
+import com.ajlopez.blockchain.core.Block;
+
+/**
+ * Created by ajlopez on 02/12/2018.
+ */
+public class BlocksProvider {
+    private final BlockChain blockChain;
+
+    public BlocksProvider(BlockChain blockChain) {
+        this.blockChain = blockChain;
+    }
+
+    public Block getBlock(String blockId) throws JsonRpcException {
+        if ("latest".equals(blockId))
+            return blockChain.getBestBlock();
+        if ("earliest".equals(blockId))
+            return blockChain.getBlockByNumber(0);
+        if ("pending".equals(blockId))
+            throw new JsonRpcException("Unsupported block id 'pending'");
+
+        return this.blockChain.getBlockByNumber(toLongNumber(blockId));
+    }
+
+    private static long toLongNumber(String text) throws JsonRpcException {
+        try {
+            if (text.startsWith("0x"))
+                return Long.parseLong(text.substring(2), 16);
+
+            return Long.parseLong(text);
+        }
+        catch (NumberFormatException ex) {
+            throw new JsonRpcException("Invalid number format");
+        }
+    }
+}
---
+++ src/test/java/com/ajlopez/blockchain/jsonrpc/BlocksProviderTest.java
@@ -0,0 +1,103 @@
+package com.ajlopez.blockchain.jsonrpc;
+
+import com.ajlopez.blockchain.bc.BlockChain;
+import com.ajlopez.blockchain.core.Block;
+import com.ajlopez.blockchain.json.JsonConverter;
+import com.ajlopez.blockchain.json.JsonValue;
+import com.ajlopez.blockchain.test.utils.FactoryHelper;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by ajlopez on 02/12/2018.
+ */
+public class BlocksProviderTest {
+    // https://www.infoq.com/news/2009/07/junit-4.7-rules
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void retrieveLatestBlock() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 10);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        Block block = provider.getBlock("latest");
+
+        Assert.assertNotNull(block);
+        Assert.assertEquals(10, block.getNumber());
+        Assert.assertEquals(blockChain.getBestBlock().getHash(), block.getHash());
+    }
+
+    @Test
+    public void retrieveEarliestBlock() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 10);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        Block block = provider.getBlock("earliest");
+
+        Assert.assertNotNull(block);
+        Assert.assertEquals(0, block.getNumber());
+        Assert.assertEquals(blockChain.getBlockByNumber(0).getHash(), block.getHash());
+    }
+
+    @Test
+    public void retrieveBlockByDecimalNumber() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 10);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        Block block = provider.getBlock("7");
+
+        Assert.assertNotNull(block);
+        Assert.assertEquals(7, block.getNumber());
+        Assert.assertEquals(blockChain.getBlockByNumber(7).getHash(), block.getHash());
+    }
+
+    @Test
+    public void retrieveBlockByHexadecimalNumber() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 20);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        Block block = provider.getBlock("0x0a");
+
+        Assert.assertNotNull(block);
+        Assert.assertEquals(10, block.getNumber());
+        Assert.assertEquals(blockChain.getBlockByNumber(10).getHash(), block.getHash());
+    }
+
+    @Test
+    public void pendingNotSupported() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 20);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        exception.expect(JsonRpcException.class);
+        exception.expectMessage("Unsupported block id 'pending'");
+        provider.getBlock("pending");
+    }
+
+    @Test
+    public void invalidNumberFormat() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 20);
+
+        BlocksProvider provider = new BlocksProvider(blockChain);
+
+        exception.expect(JsonRpcException.class);
+        exception.expectMessage("Invalid number format");
+        provider.getBlock("foo");
+    }
+}
