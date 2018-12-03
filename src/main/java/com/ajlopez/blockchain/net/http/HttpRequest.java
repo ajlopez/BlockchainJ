8c0cd2274e7d7d9e4e6e6d780c41e5b25f0de5c3
ajlopez <webmaster@ajlopez.com>
2018-12-03 11:49:25 -0300

    Create HttpRequest

---
+++ src/main/java/com/ajlopez/blockchain/net/http/HttpRequest.java
@@ -0,0 +1,24 @@
+package com.ajlopez.blockchain.net.http;
+
+import java.io.Reader;
+
+/**
+ * Created by ajlopez on 03/12/2018.
+ */
+public class HttpRequest {
+    private final String method;
+    private final String resource;
+    private final Reader reader;
+
+    public HttpRequest(String method, String resource, Reader reader) {
+        this.method = method;
+        this.resource = resource;
+        this.reader = reader;
+    }
+
+    public String getMethod() { return this.method; }
+
+    public String getResource() { return this.resource; }
+
+    public Reader getReader() { return this.reader; }
+}
---
+++ src/test/java/com/ajlopez/blockchain/net/http/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package com.ajlopez.blockchain.net.http;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+/**
+ * Created by ajlopez on 03/12/2018.
+ */
+public class HttpRequestTest {
+    @Test
+    public void createRequest() {
+        String method = "POST";
+        String resource = "/";
+        Reader reader = new StringReader("foo");
+
+        HttpRequest request = new HttpRequest(method, resource, reader);
+        
+        Assert.assertEquals(method, request.getMethod());
+        Assert.assertEquals(resource, request.getResource());
+        Assert.assertEquals(reader, request.getReader());
+    }
+}
