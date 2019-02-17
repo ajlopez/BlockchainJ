3f65c1ab71fe0d2aebe892743c2f8eba6dd5a2f8
ajlopez <webmaster@ajlopez.com>
2019-02-17 13:30:49 -0300

    TrieStorage

---
+++ src/main/java/com/ajlopez/blockchain/vms/eth/TrieStorage.java
@@ -0,0 +1,42 @@
+package com.ajlopez.blockchain.vms.eth;
+
+import com.ajlopez.blockchain.core.types.DataWord;
+import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.state.Trie;
+
+/**
+ * Created by Angel on 17/02/2019.
+ */
+public class TrieStorage implements Storage {
+    private Trie trie;
+
+    public TrieStorage(Trie trie) {
+        this.trie = trie;
+    }
+
+    @Override
+    public void setValue(DataWord address, DataWord value) {
+        byte[] bkey = address.getBytes();
+
+        if (value.equals(DataWord.ZERO))
+            this.trie = this.trie.delete(bkey);
+        else {
+            byte[] bvalue = value.toNormalizedBytes();
+            this.trie = this.trie.put(bkey, bvalue);
+        }
+    }
+
+    @Override
+    public DataWord getValue(DataWord address) {
+        byte[] data = this.trie.get(address.getBytes());
+
+        if (data == null)
+            return DataWord.ZERO;
+
+        return new DataWord(data);
+    }
+
+    public Hash getRootHash() {
+        return this.trie.getHash();
+    }
+}
---
+++ src/test/java/com/ajlopez/blockchain/vms/eth/TrieStorageTest.java
@@ -0,0 +1,71 @@
+package com.ajlopez.blockchain.vms.eth;
+
+import com.ajlopez.blockchain.core.types.DataWord;
+import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.state.Trie;
+import com.ajlopez.blockchain.store.HashMapStore;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ajlopez on 17/02/2019.
+ */
+public class TrieStorageTest {
+    @Test
+    public void getZeroIfUndefinedValue() {
+        Storage storage = new TrieStorage(new Trie());
+
+        DataWord result = storage.getValue(DataWord.fromHexadecimalString("0102"));
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(DataWord.ZERO, result);
+    }
+
+    @Test
+    public void setAndGetValue() {
+        Storage storage = new TrieStorage(new Trie());
+        DataWord address = DataWord.fromHexadecimalString("0x010203");
+        DataWord value = DataWord.fromHexadecimalString("2a");
+
+        storage.setValue(address, value);
+
+        DataWord result = storage.getValue(address);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(value, result);
+    }
+
+    @Test
+    public void setResetAndGetValue() {
+        Storage storage = new TrieStorage(new Trie());
+        DataWord address = DataWord.fromHexadecimalString("0x010203");
+        DataWord value = DataWord.fromHexadecimalString("2a");
+
+        storage.setValue(address, value);
+        storage.setValue(address, DataWord.ONE);
+
+        DataWord result = storage.getValue(address);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(DataWord.ONE, result);
+    }
+
+    @Test
+    public void setResetToZeroAndGetValue() {
+        TrieStorage storage = new TrieStorage(new Trie());
+
+        Hash initialHash = storage.getRootHash();
+
+        DataWord address = DataWord.fromHexadecimalString("0x010203");
+        DataWord value = DataWord.fromHexadecimalString("2a");
+
+        storage.setValue(address, value);
+        storage.setValue(address, DataWord.fromUnsignedInteger(0));
+
+        DataWord result = storage.getValue(address);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(DataWord.ZERO, result);
+        Assert.assertEquals(initialHash, storage.getRootHash());
+    }
+}
