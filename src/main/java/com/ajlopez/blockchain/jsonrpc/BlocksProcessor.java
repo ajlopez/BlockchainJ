dc045bf39b98eae6a1cb1ea74a70b0ab412fe38a
ajlopez <webmaster@ajlopez.com>
2019-04-21 17:09:57 -0300

    Get block by number in BlocksProcessor

--- src/main/java/com/ajlopez/blockchain/jsonrpc/BlocksProcessor.java
+++ src/main/java/com/ajlopez/blockchain/jsonrpc/BlocksProcessor.java
@@ -1,6 +1,7 @@
 package com.ajlopez.blockchain.jsonrpc;
 
 import com.ajlopez.blockchain.bc.BlockChain;
+import com.ajlopez.blockchain.jsonrpc.encoders.BlockJsonEncoder;
 
 /**
  * Created by ajlopez on 30/11/2018.
@@ -16,6 +17,9 @@ public class BlocksProcessor extends AbstractJsonRpcProcessor {
     public JsonRpcResponse processRequest(JsonRpcRequest request) throws JsonRpcException {
         if (request.check("eth_blockNumber", 0))
             return JsonRpcResponse.createResponse(request, this.blockChain.getBestBlockNumber());
+
+        if (request.check("eth_getBlockByNumber", 1))
+            return JsonRpcResponse.createResponse(request, BlockJsonEncoder.encode(this.blockChain.getBlockByNumber(Long.parseLong(request.getParams().get(0).getValue().toString()))));
 
         return super.processRequest(request);
     }
--- src/test/java/com/ajlopez/blockchain/jsonrpc/BlocksProcessorTest.java
+++ src/test/java/com/ajlopez/blockchain/jsonrpc/BlocksProcessorTest.java
@@ -1,8 +1,7 @@
 package com.ajlopez.blockchain.jsonrpc;
 
 import com.ajlopez.blockchain.bc.BlockChain;
-import com.ajlopez.blockchain.json.JsonConverter;
-import com.ajlopez.blockchain.json.JsonValue;
+import com.ajlopez.blockchain.json.*;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -54,6 +53,30 @@ public class BlocksProcessorTest {
         Assert.assertEquals(request.getId(), response.getId());
         Assert.assertEquals(request.getJsonRpc(), response.getJsonRpc());
         Assert.assertEquals("\"0x0a\"", response.getResult().toString());
+    }
+
+    @Test
+    public void getBlockByNumberUsingBlockchainWithTenBlocks() throws JsonRpcException {
+        BlockChain blockChain = FactoryHelper.createBlockChainWithGenesis();
+        FactoryHelper.extendBlockChainWithBlocks(blockChain, 10);
+
+        List<JsonValue> params = new ArrayList<>();
+        params.add(new JsonNumericValue(10));
+        JsonRpcRequest request =  new JsonRpcRequest("1", "2.0", "eth_getBlockByNumber", params);
+
+        BlocksProcessor processor = new BlocksProcessor(blockChain);
+
+        JsonRpcResponse response = processor.processRequest(request);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(request.getId(), response.getId());
+        Assert.assertEquals(request.getJsonRpc(), response.getJsonRpc());
+        Assert.assertEquals(JsonValueType.OBJECT, response.getResult().getType());
+
+        JsonObjectValue jovalue = (JsonObjectValue)response.getResult();
+
+        Assert.assertTrue(jovalue.hasProperty("number"));
+        Assert.assertEquals("10", jovalue.getProperty("number").getValue());
     }
 
     @Test
