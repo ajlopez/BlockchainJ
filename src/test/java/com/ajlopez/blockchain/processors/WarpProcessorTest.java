4eab38a7b812f438d536f332c0ef3b3de3f5498d
ajlopez <webmaster@ajlopez.com>
2019-06-28 13:42:02 -0300

    WarpProcessor copy account state test, WIP

--- src/test/java/com/ajlopez/blockchain/processors/WarpProcessorTest.java
+++ src/test/java/com/ajlopez/blockchain/processors/WarpProcessorTest.java
@@ -1,8 +1,12 @@
 package com.ajlopez.blockchain.processors;
 
 import com.ajlopez.blockchain.bc.GenesisGenerator;
+import com.ajlopez.blockchain.core.Account;
 import com.ajlopez.blockchain.core.Block;
+import com.ajlopez.blockchain.core.Transaction;
 import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.execution.AccountState;
+import com.ajlopez.blockchain.store.AccountStore;
 import com.ajlopez.blockchain.store.HashMapStore;
 import com.ajlopez.blockchain.store.KeyValueStore;
 import com.ajlopez.blockchain.store.TrieStore;
@@ -89,5 +93,35 @@ public class WarpProcessorTest {
         Assert.assertNotNull(result);
         Assert.assertFalse(result.isEmpty());
         Assert.assertTrue(accountStore.exists(block.getStateRootHash()));
+    }
+
+    @Test
+    public void copyCompleteStateOfBlockWithTransactions() {
+        KeyValueStore keyValueStore0 = new HashMapStore();
+        TrieStore trieStore0 = new TrieStore(keyValueStore0);
+
+        Block block = FactoryHelper.createBlockChain(trieStore0, 1, 10).getBlockByNumber(1);
+
+        Assert.assertTrue(trieStore0.exists(block.getStateRootHash()));
+
+        TrieStore accountStore = new TrieStore(new HashMapStore());
+
+        WarpProcessor processor = new WarpProcessor(accountStore);
+
+        processor.processBlock(block);
+
+        Hash topHash = block.getStateRootHash();
+
+        for (Set<Hash> pendingHashes = processor.getPendingAccountHashes(topHash); !pendingHashes.isEmpty(); pendingHashes = processor.getPendingAccountHashes(topHash))
+            for (Hash pendingHash : pendingHashes)
+                processor.processAccountNode(topHash, trieStore0.retrieve(pendingHash).getEncoded());
+
+        AccountStore accounts = new AccountStore(accountStore.retrieve(topHash));
+        
+        for (Transaction transaction : block.getTransactions()) {
+            Account account = accounts.getAccount(transaction.getReceiver());
+            Assert.assertEquals(transaction.getValue(), account.getBalance());
+            Assert.assertEquals(0, account.getNonce());
+        }
     }
 }
