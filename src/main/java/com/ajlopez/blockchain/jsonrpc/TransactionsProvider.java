3ad62adc95f31a72b385bc5ddbc89852be5eb2ba
ajlopez <webmaster@ajlopez.com>
2019-08-21 11:59:38 -0300

    TransactionsProvider get transaction

--- src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProvider.java
+++ src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProvider.java
@@ -1,7 +1,11 @@
 package com.ajlopez.blockchain.jsonrpc;
 
 import com.ajlopez.blockchain.core.Transaction;
+import com.ajlopez.blockchain.core.types.Hash;
 import com.ajlopez.blockchain.processors.TransactionPool;
+import com.ajlopez.blockchain.utils.HexUtils;
+
+import java.util.List;
 
 /**
  * Created by ajlopez on 20/08/2019.
@@ -14,6 +18,15 @@ public class TransactionsProvider {
     }
 
     public Transaction getTransaction(String txid) {
+        Hash hash = new Hash(HexUtils.hexStringToBytes(txid));
+
+        // TODO improve, add map in transaction pool
+        List<Transaction> transactions = this.transactionPool.getTransactions();
+
+        for (Transaction transaction : transactions)
+            if (transaction.getHash().equals(hash))
+                return transaction;
+
         return null;
     }
 }
--- src/test/java/com/ajlopez/blockchain/jsonrpc/TransactionsProviderTest.java
+++ src/test/java/com/ajlopez/blockchain/jsonrpc/TransactionsProviderTest.java
@@ -1,6 +1,8 @@
 package com.ajlopez.blockchain.jsonrpc;
 
+import com.ajlopez.blockchain.core.Transaction;
 import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.processors.TransactionPool;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,11 +13,29 @@ import org.junit.Test;
 public class TransactionsProviderTest {
     @Test
     public void getUnknownTransactionAsNull() {
+        TransactionPool transactionPool = new TransactionPool();
         Hash hash = FactoryHelper.createRandomHash();
         String txid = hash.toString();
 
-        TransactionsProvider transactionsProvider = new TransactionsProvider(null);
+        TransactionsProvider transactionsProvider = new TransactionsProvider(transactionPool);
 
         Assert.assertNull(transactionsProvider.getTransaction(txid));
+    }
+
+    @Test
+    public void getTransaction() {
+        TransactionPool transactionPool = new TransactionPool();
+        Transaction transaction = FactoryHelper.createTransaction(1000);
+        transactionPool.addTransaction(transaction);
+
+        Hash hash = transaction.getHash();
+        String txid = hash.toString();
+
+        TransactionsProvider transactionsProvider = new TransactionsProvider(transactionPool);
+
+        Transaction result = transactionsProvider.getTransaction(txid);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(transaction, result);
     }
 }
