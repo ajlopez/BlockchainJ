1083ab39e7d0b318747034b727bf519822f8d212
ajlopez <webmaster@ajlopez.com>
2019-08-27 12:23:09 -0300

    TransactionsProcessor JSON first test

--- src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProcessor.java
+++ src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProcessor.java
@@ -1,7 +1,19 @@
 package com.ajlopez.blockchain.jsonrpc;
 
+import com.ajlopez.blockchain.json.JsonNullValue;
+
 /**
  * Created by ajlopez on 20/08/2019.
  */
 public class TransactionsProcessor extends AbstractJsonRpcProcessor {
+    private final TransactionsProvider transactionsProvider;
+
+    public TransactionsProcessor(TransactionsProvider transactionsProvider) {
+        this.transactionsProvider = transactionsProvider;
+    }
+
+    @Override
+    public JsonRpcResponse processRequest(JsonRpcRequest request) throws JsonRpcException {
+        return JsonRpcResponse.createResponse(request, JsonNullValue.getInstance());
+    }
 }
---
+++ src/test/java/com/ajlopez/blockchain/jsonrpc/TransactionsProcessorTest.java
@@ -0,0 +1,35 @@
+package com.ajlopez.blockchain.jsonrpc;
+
+import com.ajlopez.blockchain.json.JsonStringValue;
+import com.ajlopez.blockchain.json.JsonValue;
+import com.ajlopez.blockchain.json.JsonValueType;
+import com.ajlopez.blockchain.processors.TransactionPool;
+import com.ajlopez.blockchain.test.utils.FactoryHelper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ajlopez on 27/08/2019.
+ */
+public class TransactionsProcessorTest {
+    @Test
+    public void unknownTransaction() throws JsonRpcException {
+        TransactionPool transactionPool = new TransactionPool();
+        TransactionsProvider transactionsProvider = new TransactionsProvider(transactionPool);
+
+        TransactionsProcessor transactionsProcessor = new TransactionsProcessor(transactionsProvider);
+
+        List<JsonValue> params = new ArrayList<>();
+        params.add(new JsonStringValue(FactoryHelper.createRandomHash().toString()));
+        JsonRpcRequest request =  new JsonRpcRequest("1", "2.0", "eth_getTransactionByHash", params);
+
+        JsonRpcResponse response = transactionsProcessor.processRequest(request);
+
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getResult());
+        Assert.assertEquals(JsonValueType.NULL, response.getResult().getType());
+    }
+}
