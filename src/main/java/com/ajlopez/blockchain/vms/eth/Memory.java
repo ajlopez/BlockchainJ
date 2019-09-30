0fa74fa5bf70a1aac83b146f38a575eea5e21b3f
ajlopez <webmaster@ajlopez.com>
2019-09-30 09:16:04 -0300

    Virtual machine memory refactor, using byte chunks, WIP

--- src/main/java/com/ajlopez/blockchain/vms/eth/Memory.java
+++ src/main/java/com/ajlopez/blockchain/vms/eth/Memory.java
@@ -2,35 +2,47 @@ package com.ajlopez.blockchain.vms.eth;
 
 import com.ajlopez.blockchain.core.types.DataWord;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ajlopez on 11/12/2018.
  */
 public class Memory {
-    // TODO implement array of memory chunks
-    private byte[] bytes;
+    public static final int CHUNK_SIZE = 4096;
+    private final List<byte[]> chunks = new ArrayList<>();
+    private int size;
 
     public void setValue(int address, DataWord value) {
         ensureSize(address + DataWord.DATAWORD_BYTES);
 
-        System.arraycopy(value.getBytes(), 0, this.bytes, address, DataWord.DATAWORD_BYTES);
+        int nchunk = address / CHUNK_SIZE;
+        int choffset = address % CHUNK_SIZE;
+
+        // TODO Control chunk crossing
+        System.arraycopy(value.getBytes(), 0, this.chunks.get(nchunk), choffset, DataWord.DATAWORD_BYTES);
     }
 
     public DataWord getValue(int address) {
-        int size = this.size();
+        if (address >= this.size)
+            return DataWord.ZERO;
 
-        if (address + DataWord.DATAWORD_BYTES <= size)
-            return DataWord.fromBytes(this.bytes, address, DataWord.DATAWORD_BYTES);
+        int nchunk = address / CHUNK_SIZE;
+        int choffset = address % CHUNK_SIZE;
 
-        if (address < size)
-            return DataWord.fromBytesToLeft(this.bytes, address, size - address);
+        if (address + DataWord.DATAWORD_BYTES <= this.size)
+            return DataWord.fromBytes(this.chunks.get(nchunk), choffset, DataWord.DATAWORD_BYTES);
 
-        return DataWord.ZERO;
+        return DataWord.fromBytesToLeft(this.chunks.get(nchunk), choffset, this.size - address);
     }
 
     public void setByte(int address, byte value) {
         ensureSize(address + 1);
 
-        this.bytes[address] = value;
+        int nchunk = address / CHUNK_SIZE;
+        int choffset = address % CHUNK_SIZE;
+
+        this.chunks.get(nchunk)[choffset] = value;
     }
 
     public void setBytes(int address, byte[] bytes, int offset, int length) {
@@ -39,40 +51,39 @@ public class Memory {
         if (bytes.length <= offset)
             return;
 
-        System.arraycopy(bytes, offset, this.bytes, address, Math.min(bytes.length - offset, length));
+        int nchunk = address / CHUNK_SIZE;
+        int choffset = address % CHUNK_SIZE;
+
+        // TODO Control chunk crossing
+        System.arraycopy(bytes, offset, this.chunks.get(nchunk), choffset, Math.min(bytes.length - offset, length));
     }
 
     public byte[] getBytes(int address, int length) {
         byte[] bytes = new byte[length];
 
-        if (this.bytes == null)
+        if (this.size < address)
             return bytes;
 
-        System.arraycopy(this.bytes, address, bytes, 0, Math.min(this.bytes.length - address, length));
+        int nchunk = address / CHUNK_SIZE;
+        int offset = address % CHUNK_SIZE;
+
+        // TODO Control chunk crossing
+        System.arraycopy(this.chunks.get(nchunk), offset, bytes, 0, Math.min(this.size - address, length));
 
         return bytes;
     }
 
     public int size() {
-        if (this.bytes == null)
-            return 0;
-
-        return this.bytes.length;
+        return this.size;
     }
 
     private void ensureSize(int size) {
-        if (this.bytes == null) {
-            this.bytes = new byte[size];
-            return;
-        }
-
-        if (this.bytes.length >= size)
+        if (size <= this.size)
             return;
 
-        byte[] newbytes = new byte[size];
+        while (this.chunks.size() * CHUNK_SIZE < size)
+            this.chunks.add(new byte[CHUNK_SIZE]);
 
-        System.arraycopy(this.bytes, 0, newbytes, 0, this.bytes.length);
-
-        this.bytes = newbytes;
+        this.size = size;
     }
 }
