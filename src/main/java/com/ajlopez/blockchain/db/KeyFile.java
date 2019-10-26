8e64a013bb7ab20d8a68598e95d7d1cfd776c8ca
ajlopez <webmaster@ajlopez.com>
2019-10-26 12:50:17 -0300

    KeyFile refactor, using a map for keys and info

--- src/main/java/com/ajlopez/blockchain/db/KeyFile.java
+++ src/main/java/com/ajlopez/blockchain/db/KeyFile.java
@@ -2,12 +2,15 @@ package com.ajlopez.blockchain.db;
 
 import com.ajlopez.blockchain.store.HashMapStore;
 import com.ajlopez.blockchain.store.KeyValueStore;
+import com.ajlopez.blockchain.utils.ByteArrayWrapper;
 import com.ajlopez.blockchain.utils.ByteUtils;
 import com.sun.javaws.exceptions.InvalidArgumentException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ajlopez on 21/10/2019.
@@ -15,10 +18,27 @@ import java.io.RandomAccessFile;
 public class KeyFile {
     private final RandomAccessFile file;
     private final int keyLength;
+    private Map<ByteArrayWrapper, ValueInfo> keys = new HashMap<>();
 
-    public KeyFile(String name, int keyLength) throws FileNotFoundException {
+    public KeyFile(String name, int keyLength) throws IOException {
         this.file = new RandomAccessFile(name, "rw");
         this.keyLength = keyLength;
+
+        int blockSize = this.getBlockSize();
+        long nkeys = this.getNoKeys(blockSize);
+
+        this.file.seek(0);
+
+        for (long k = 0; k < nkeys; k++) {
+            byte[] buffer = new byte[this.keyLength];
+
+            this.file.read(buffer);
+
+            long position = this.file.readLong();
+            int length = this.file.readInt();
+
+            this.keys.put(new ByteArrayWrapper(buffer), new ValueInfo(position, length));
+        }
     }
 
     private int getBlockSize() {
@@ -39,26 +59,12 @@ public class KeyFile {
         this.file.write(key);
         this.file.writeLong(position);
         this.file.writeInt(length);
+
+        this.keys.put(new ByteArrayWrapper(key), new ValueInfo(position, length));
     }
 
     public ValueInfo readKey(byte[] key) throws IOException {
-        this.file.seek(0L);
-
-        byte[] buffer = new byte[this.keyLength];
-
-        while (true) {
-            int nbytes = this.file.read(buffer);
-
-            if (nbytes != this.keyLength)
-                return null;
-
-            long position = this.file.readLong();
-            int length = this.file.readInt();
-
-            if (ByteUtils.equals(key, 0, buffer, 0, this.keyLength)) {
-                return new ValueInfo(position, length);
-            }
-        }
+        return this.keys.get(new ByteArrayWrapper(key));
     }
 
     public void close() throws IOException {
