7cbc8ee7748edf1545ecf0e5950cc37a2bc39c47
ajlopez <webmaster@ajlopez.com>
2020-01-19 10:13:48 -0300

    TrieKeyUtils get subkey

--- src/main/java/com/ajlopez/blockchain/state/TrieKeyUtils.java
+++ src/main/java/com/ajlopez/blockchain/state/TrieKeyUtils.java
@@ -10,4 +10,27 @@ public class TrieKeyUtils {
 
         return key[position / 2] & 0x0f;
     }
+
+    public static byte[] getSubKey(byte[] key, int from, int length) {
+        byte[] subkey = new byte[(length + 1) / 2];
+
+        if (from % 2 == 0)
+            for (int k = 0; k < length / 2; k++)
+                subkey[k] = key[from / 2 + k];
+        else
+            for (int k = 0; k < length; k++) {
+                int position = from + k;
+                int offset = getOffset(key, position);
+
+                if (k % 2 == 0)
+                    subkey[k / 2] = (byte)(offset << 4);
+                else
+                    subkey[k / 2] |= (byte)offset;
+            }
+
+        if (length % 2 == 1)
+            subkey[subkey.length - 1] = (byte)(getOffset(key, from + length - 1) << 4);
+
+        return subkey;
+    }
 }
--- src/test/java/com/ajlopez/blockchain/state/TrieKeyUtilsTest.java
+++ src/test/java/com/ajlopez/blockchain/state/TrieKeyUtilsTest.java
@@ -18,4 +18,21 @@ public class TrieKeyUtilsTest {
         Assert.assertEquals(15, TrieKeyUtils.getOffset(key, 4));
         Assert.assertEquals(14, TrieKeyUtils.getOffset(key, 5));
     }
+
+    @Test
+    public void getSubKey() {
+        byte[] key = { 0x12, 0x34, (byte)0xfe };
+
+        byte[] skey01 = { 0x10 };
+        byte[] skey02 = { 0x12 };
+        byte[] skey11 = { 0x20 };
+        byte[] skey12 = { 0x23 };
+        byte[] skey33 = { 0x4f, (byte)0xe0 };
+
+        Assert.assertArrayEquals(skey01, TrieKeyUtils.getSubKey(key, 0, 1));
+        Assert.assertArrayEquals(skey02, TrieKeyUtils.getSubKey(key, 0, 2));
+        Assert.assertArrayEquals(skey11, TrieKeyUtils.getSubKey(key, 1, 1));
+        Assert.assertArrayEquals(skey12, TrieKeyUtils.getSubKey(key, 1, 2));
+        Assert.assertArrayEquals(skey33, TrieKeyUtils.getSubKey(key, 3, 3));
+    }
 }
