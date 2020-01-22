edfa6aa2e0e68c4c7c27ee5b9a9de02465d2ee00
ajlopez <webmaster@ajlopez.com>
2020-01-22 17:07:35 -0300

    TrieKeyUtils get shared key length

--- src/test/java/com/ajlopez/blockchain/state/TrieKeyUtilsTest.java
+++ src/test/java/com/ajlopez/blockchain/state/TrieKeyUtilsTest.java
@@ -50,7 +50,7 @@ public class TrieKeyUtilsTest {
         Assert.assertEquals(2, TrieKeyUtils.getSharedLength(skey02, 2, key, 0));
         Assert.assertEquals(1, TrieKeyUtils.getSharedLength(skey11, 1, key, 1));
         Assert.assertEquals(2, TrieKeyUtils.getSharedLength(skey12, 2, key, 1));
-        Assert.assertEquals(2, TrieKeyUtils.getSharedLength(skey33, 3, key, 3));
+        Assert.assertEquals(3, TrieKeyUtils.getSharedLength(skey33, 3, key, 3));
         Assert.assertEquals(0, TrieKeyUtils.getSharedLength(skey33, 3, key, 0));
     }
 }
