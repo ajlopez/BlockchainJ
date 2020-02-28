cb39dc5e3910f2ca3a7ae39d649c42c49808da4e
ajlopez <webmaster@ajlopez.com>
2020-02-28 09:15:44 -0300

    RpcRunner get network version test

--- src/test/java/com/ajlopez/blockchain/RpcRunnerTest.java
+++ src/test/java/com/ajlopez/blockchain/RpcRunnerTest.java
@@ -1,6 +1,7 @@
 package com.ajlopez.blockchain;
 
 import com.ajlopez.blockchain.bc.BlockChain;
+import com.ajlopez.blockchain.config.NetworkConfiguration;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -63,6 +64,34 @@ public class RpcRunnerTest {
         socket.close();
 
         Assert.assertNotNull(result);
-        Assert.assertEquals("HTTP/1.1 200 OK\r\n\r\n{ \"id\": \"1\", \"jsonrpc\": \"2.0\", \"result\": \"0x0a\" }", result);        
+        Assert.assertEquals("HTTP/1.1 200 OK\r\n\r\n{ \"id\": \"1\", \"jsonrpc\": \"2.0\", \"result\": \"0x0a\" }", result);
+    }
+
+    @Test
+    public void getNetworkVersion() throws IOException {
+        NetworkConfiguration networkConfiguration = new NetworkConfiguration((short)42);
+
+        RpcRunner rpcRunner = new RpcRunner(6002, null, networkConfiguration);
+
+        rpcRunner.start();
+
+        Socket socket = new Socket("127.0.0.1", 6002);
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+        String request = "POST /\r\n\r\n{ \"id\": 1, \"jsonrpc\": \"2.0\", \"method\": \"net_version\", \"params\": [] }";
+        writer.println(request);
+        writer.flush();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String result = reader.readLine() + "\r\n"
+                + reader.readLine() + "\r\n"
+                + reader.readLine();
+
+        rpcRunner.stop();
+        socket.close();
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals("HTTP/1.1 200 OK\r\n\r\n{ \"id\": \"1\", \"jsonrpc\": \"2.0\", \"result\": \"0x2a\" }", result);
     }
 }
