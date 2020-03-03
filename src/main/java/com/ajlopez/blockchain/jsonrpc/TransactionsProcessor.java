098c93dea6d3ab42cc391edf247cda806bacf9e1
ajlopez <webmaster@ajlopez.com>
2020-03-03 09:58:08 -0300

    Transactions processor refactor

--- src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProcessor.java
+++ src/main/java/com/ajlopez/blockchain/jsonrpc/TransactionsProcessor.java
@@ -25,26 +25,32 @@ public class TransactionsProcessor extends AbstractJsonRpcProcessor {
 
     @Override
     public JsonRpcResponse processRequest(JsonRpcRequest request) throws JsonRpcException, IOException {
-        if (request.check("eth_getTransactionByHash", 1)) {
-            String txid = request.getParams().get(0).getValue().toString();
-            Transaction transaction = this.transactionsProvider.getTransaction(txid);
-            return JsonRpcResponse.createResponse(request, TransactionJsonEncoder.encode(transaction, true));
-        }
+        if (request.check("eth_getTransactionByHash", 1))
+            return getTransactionByHash(request);
 
-        if (request.check("eth_sendTransaction", 1)) {
-            JsonValue jvalue = request.getParams().get(0);
-            Transaction transaction = TransactionJsonEncoder.decode(jvalue);
-
-            if (!((JsonObjectValue)jvalue).hasProperty("nonce")) {
-                Account sender = this.accountsProvider.getAccount(transaction.getSender(), "latest");
-                transaction = transaction.withNonce(this.transactionsProvider.getNextNonce(transaction.getSender(), sender.getNonce()));
-            }
-
-            // TODO how to relay the transaction, message/receiver processor instead of transaction processor?
-            this.transactionProcessor.processTransaction(transaction);
-            return JsonRpcResponse.createResponse(request, transaction.getHash().toString());
-        }
+        if (request.check("eth_sendTransaction", 1))
+            return sendTransaction(request);
 
         return super.processRequest(request);
+    }
+
+    private JsonRpcResponse getTransactionByHash(JsonRpcRequest request) {
+        String txid = request.getParams().get(0).getValue().toString();
+        Transaction transaction = this.transactionsProvider.getTransaction(txid);
+        return JsonRpcResponse.createResponse(request, TransactionJsonEncoder.encode(transaction, true));
+    }
+
+    private JsonRpcResponse sendTransaction(JsonRpcRequest request) throws JsonRpcException, IOException {
+        JsonValue jvalue = request.getParams().get(0);
+        Transaction transaction = TransactionJsonEncoder.decode(jvalue);
+
+        if (!((JsonObjectValue)jvalue).hasProperty("nonce")) {
+            Account sender = this.accountsProvider.getAccount(transaction.getSender(), "latest");
+            transaction = transaction.withNonce(this.transactionsProvider.getNextNonce(transaction.getSender(), sender.getNonce()));
+        }
+
+        // TODO how to relay the transaction, message/receiver processor instead of transaction processor?
+        this.transactionProcessor.processTransaction(transaction);
+        return JsonRpcResponse.createResponse(request, transaction.getHash().toString());
     }
 }
