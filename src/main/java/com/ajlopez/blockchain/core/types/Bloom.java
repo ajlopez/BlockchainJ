6139296c35d81f2d9490ab53a9fce982a20a8142
ajlopez <webmaster@ajlopez.com>
2020-04-10 13:05:21 -0300

    First Bloom tests and implementation

--- src/main/java/com/ajlopez/blockchain/core/types/Bloom.java
+++ src/main/java/com/ajlopez/blockchain/core/types/Bloom.java
@@ -1,7 +1,54 @@
 package com.ajlopez.blockchain.core.types;
 
 /**
- * Created by Angel on 4/10/2020.
+ * Created by ajlopez on 10/04/2020.
  */
 public class Bloom {
+    public static final int BLOOM_BYTES = 256;
+    public static final int BLOOM_BITS = BLOOM_BYTES * 8;
+
+    private final byte[] data = new byte[BLOOM_BYTES];
+
+    public Bloom() {
+    }
+
+    public int size() {
+        int count = 0;
+
+        for (int k = 0; k < BLOOM_BYTES; k++) {
+            byte b = this.data[k];
+
+            if (b == 0)
+                continue;
+
+            for (int j = 0; j < 8; j++) {
+                if ((b & 1) == 1)
+                    count++;
+
+                b >>= 1;
+            }
+        }
+
+        return count;
+    }
+
+    public void add(int nelement) {
+        if (nelement < 0 || nelement >= BLOOM_BITS)
+            throw new IllegalArgumentException("Invalid bloom element");
+
+        int position = nelement / 8;
+        int offset = 7 - (nelement % 8);
+
+        data[position] |= 1 << offset;
+    }
+
+    public boolean include(Bloom bloom) {
+        byte[] bdata = bloom.data;
+
+        for (int k = 0; k < BLOOM_BYTES; k++)
+            if ((bdata[k] & this.data[k]) != bdata[k])
+                return false;
+
+        return true;
+    }
 }
--- src/test/java/com/ajlopez/blockchain/core/types/BloomTest.java
+++ src/test/java/com/ajlopez/blockchain/core/types/BloomTest.java
@@ -1,7 +1,101 @@
 package com.ajlopez.blockchain.core.types;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Random;
+
 /**
- * Created by Angel on 4/10/2020.
+ * Created by ajlopez on 10/04/2020.
  */
 public class BloomTest {
+    // https://www.infoq.com/news/2009/07/junit-4.7-rules
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void createEmptyBloom() {
+        Bloom bloom = new Bloom();
+
+        Assert.assertEquals(0, bloom.size());
+        Assert.assertTrue(bloom.include(bloom));
+    }
+
+    @Test
+    public void include() {
+        Bloom bloom1 = new Bloom();
+        bloom1.add(1);
+        bloom1.add(42);
+        bloom1.add(100);
+
+        Bloom bloom2 = new Bloom();
+        bloom1.add(42);
+
+        Assert.assertTrue(bloom1.include(bloom1));
+        Assert.assertTrue(bloom2.include(bloom2));
+        Assert.assertTrue(bloom1.include(bloom2));
+        Assert.assertFalse(bloom2.include(bloom1));
+    }
+
+    @Test
+    public void includeWithRandomElements() {
+        Random random = new Random();
+
+        Bloom bloom1 = new Bloom();
+        Bloom bloom2 = new Bloom();
+
+        for (int k = 0; k < Bloom.BLOOM_BITS / 4; k++) {
+            int nelement = random.nextInt(Bloom.BLOOM_BITS);
+
+            bloom1.add(nelement);
+            bloom2.add(nelement);
+        }
+
+        for (int k = 0; k < Bloom.BLOOM_BITS / 4; k++)
+            bloom1.add(random.nextInt(Bloom.BLOOM_BITS));
+
+        Assert.assertTrue(bloom1.include(bloom1));
+        Assert.assertTrue(bloom2.include(bloom2));
+        Assert.assertTrue(bloom1.include(bloom2));
+        Assert.assertFalse(bloom2.include(bloom1));
+    }
+
+    @Test
+    public void addElement() {
+        Bloom bloom = new Bloom();
+
+        bloom.add(42);
+
+        Assert.assertEquals(1, bloom.size());
+    }
+
+    @Test
+    public void addInvalidElement() {
+        Bloom bloom = new Bloom();
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Invalid bloom element");
+        bloom.add(Bloom.BLOOM_BITS);
+    }
+
+    @Test
+    public void addInvalidNegativeElement() {
+        Bloom bloom = new Bloom();
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Invalid bloom element");
+        bloom.add(-1);
+    }
+
+    @Test
+    public void addAllElements() {
+        Bloom bloom = new Bloom();
+
+        for (int k = 0; k < Bloom.BLOOM_BYTES * 8; k++) {
+            bloom.add(k);
+            Assert.assertEquals(1 + k, bloom.size());
+        }
+    }
 }
