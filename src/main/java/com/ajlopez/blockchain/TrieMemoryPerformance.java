31e742aafd37916c168de1e4861b31dab2c98e55
ajlopez <webmaster@ajlopez.com>
2020-04-19 13:13:20 -0300

    Trie memory and performance main

--- src/main/java/com/ajlopez/blockchain/TrieMemoryPerformance.java
+++ src/main/java/com/ajlopez/blockchain/TrieMemoryPerformance.java
@@ -1,7 +1,99 @@
 package com.ajlopez.blockchain;
 
+import com.ajlopez.blockchain.config.ArgumentsProcessor;
+import com.ajlopez.blockchain.state.Trie;
+
+import java.io.IOException;
+import java.util.Random;
+
 /**
- * Created by Angel on 4/19/2020.
+ * Created by Angel on 19/04/2020.
  */
 public class TrieMemoryPerformance {
+    private static final Random random = new Random();
+
+    public static void main(String[] args) throws IOException {
+        ArgumentsProcessor argumentsProcessor = new ArgumentsProcessor();
+
+        argumentsProcessor.processArguments(args);
+
+        int noValues = argumentsProcessor.getInteger(0);
+        int keySize = argumentsProcessor.getInteger(1);
+        int valueSize = argumentsProcessor.getInteger(2);
+
+        CollectedData collectedData = new CollectedData();
+
+        collectedData.noValues = noValues;
+        collectedData.keySize = keySize;
+        collectedData.valueSize = valueSize;
+
+        Trie trie = new Trie();
+
+        collectedData.megaBytesBefore = (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+
+        long millis = System.currentTimeMillis();
+
+        for (int k = 0; k < noValues; k++) {
+            byte[] key = createRandomBytes(keySize);
+            byte[] value = createRandomBytes(valueSize);
+
+            trie = trie.put(key, value);
+        }
+
+        collectedData.creationTime = System.currentTimeMillis() - millis;
+        collectedData.trieSize = trie.nodesSize();
+
+        // System.out.println("Creation time (ms): " + millis);
+
+        // System.out.println("Trie size (nodes): " + trie.nodesSize());
+
+        collectedData.megaBytesAfter = (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+        // System.out.println("MB after: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024));
+
+        System.gc();
+
+        collectedData.megaBytesAfterGC = (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+        //System.out.println("MB after gc: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024));
+
+        long millis2 = System.currentTimeMillis();
+
+        trie.getHash();
+
+        collectedData.hashTime = System.currentTimeMillis() - millis2;
+
+        System.out.println("No values: " + collectedData.noValues);
+        System.out.println("Key size: " + collectedData.keySize);
+        System.out.println("Value size: " + collectedData.valueSize);
+
+        System.out.println("MB before: " + collectedData.megaBytesBefore);
+        System.out.println("MB after: " + collectedData.megaBytesAfter);
+        System.out.println("MB after GC: " + collectedData.megaBytesAfterGC);
+
+        System.out.println("Creation time (ms): " + collectedData.creationTime);
+        System.out.println("Hash time (ms): " + collectedData.hashTime);
+
+        System.out.println("Trie size (nodes): " + collectedData.trieSize);
+    }
+
+    private static byte[] createRandomBytes(int length) {
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+
+        return bytes;
+    }
+
+    private static class CollectedData {
+        public int noValues;
+        public int keySize;
+        public int valueSize;
+
+        public double megaBytesBefore;
+        public double megaBytesAfter;
+        public double megaBytesAfterGC;
+
+        public long creationTime;
+        public long hashTime;
+
+        public int trieSize;
+    }
 }
