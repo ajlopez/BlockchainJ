4fbf9fe0090a64ad634eb8bf6c5aac33f5de174a
ajlopez <webmaster@ajlopez.com>
2020-07-02 15:32:13 -0300

    TransactionProcessor validates transaction before adding it to TransactionPool

--- src/main/java/com/ajlopez/blockchain/processors/TransactionProcessor.java
+++ src/main/java/com/ajlopez/blockchain/processors/TransactionProcessor.java
@@ -2,20 +2,24 @@ package com.ajlopez.blockchain.processors;
 
 import com.ajlopez.blockchain.core.Transaction;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by ajlopez on 27/01/2018.
  */
 public class TransactionProcessor {
-    TransactionPool transactionPool;
+    private final TransactionPool transactionPool;
+    private final TransactionValidator transactionValidator = new TransactionValidator();
 
     public TransactionProcessor(TransactionPool transactionPool) {
         this.transactionPool = transactionPool;
     }
 
     public List<Transaction> processTransaction(Transaction transaction) {
-        // TODO add transaction validation
+        if (!this.transactionValidator.isValid(transaction))
+            return Collections.emptyList();
+
         return this.transactionPool.addTransaction(transaction);
     }
 }
--- src/test/java/com/ajlopez/blockchain/processors/TransactionProcessorTest.java
+++ src/test/java/com/ajlopez/blockchain/processors/TransactionProcessorTest.java
@@ -1,9 +1,13 @@
 package com.ajlopez.blockchain.processors;
 
 import com.ajlopez.blockchain.core.Transaction;
+import com.ajlopez.blockchain.core.types.Address;
+import com.ajlopez.blockchain.core.types.Coin;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by ajlopez on 27/01/2018.
@@ -19,5 +23,25 @@ public class TransactionProcessorTest {
         processor.processTransaction(transaction);
 
         Assert.assertTrue(pool.getTransactions().contains(transaction));
+    }
+
+    @Test
+    public void rejectInvalidTransaction() {
+        Address sender = FactoryHelper.createRandomAddress();
+        Address receiver = Address.ADDRESS_RICH_TRANSACTION;
+        Coin value = Coin.ONE;
+        byte []data = FactoryHelper.createRandomBytes(42);
+
+        Transaction transaction = new Transaction(sender, receiver, value, 42, data, 6000000, Coin.ZERO);
+
+        TransactionPool pool = new TransactionPool();
+        TransactionProcessor processor = new TransactionProcessor(pool);
+
+        List<Transaction> processed = processor.processTransaction(transaction);
+
+        Assert.assertNotNull(processed);
+        Assert.assertTrue(processed.isEmpty());
+
+        Assert.assertFalse(pool.getTransactions().contains(transaction));
     }
 }
