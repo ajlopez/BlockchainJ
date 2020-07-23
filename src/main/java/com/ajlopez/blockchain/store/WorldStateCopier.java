f54cb6b3c29581201b855f2d59cfd1713b44f403
ajlopez <webmaster@ajlopez.com>
2020-07-23 15:03:13 -0300

    WorldStateCopier copies accounts and storage cells

--- src/main/java/com/ajlopez/blockchain/store/WorldStateCopier.java
+++ src/main/java/com/ajlopez/blockchain/store/WorldStateCopier.java
@@ -1,10 +1,11 @@
 package com.ajlopez.blockchain.store;
 
+import com.ajlopez.blockchain.core.Account;
 import com.ajlopez.blockchain.core.types.Hash;
+import com.ajlopez.blockchain.encoding.AccountEncoder;
 import com.ajlopez.blockchain.state.Trie;
 
 import java.io.IOException;
-import java.security.Key;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,26 +13,33 @@ import java.util.Queue;
  * Created by ajlopez on 19/07/2020.
  */
 public class WorldStateCopier {
-    private final Stores sourceStores;
-    private final Stores targetStores;
     private final TrieStore sourceAccountTrieStore;
     private final TrieStore targetAccountTrieStore;
+    private final TrieStore sourceStorageTrieStore;
+    private final TrieStore targetStorageTrieStore;
     private final Queue<KeyInformation> hashes = new LinkedList<>();
 
     public WorldStateCopier(Stores sourceStores, Stores targetStores, Hash rootHash) {
-        this.sourceStores = sourceStores;
-        this.targetStores = targetStores;
         this.sourceAccountTrieStore = sourceStores.getAccountTrieStore();
         this.targetAccountTrieStore = targetStores.getAccountTrieStore();
+        this.sourceStorageTrieStore = sourceStores.getStorageTrieStore();
+        this.targetStorageTrieStore = targetStores.getStorageTrieStore();
+
         this.hashes.add(new KeyInformation(KeyValueStoreType.ACCOUNTS, rootHash));
     }
 
     public void process() throws IOException {
-        while (!this.hashes.isEmpty())
-            processHash(this.hashes.poll().getHash());
+        while (!this.hashes.isEmpty()) {
+            KeyInformation keyInformation = this.hashes.poll();
+
+            if (keyInformation.getKeyValueStoreType() == KeyValueStoreType.ACCOUNTS)
+                processAccountNodeHash(keyInformation.getHash());
+            else if (keyInformation.getKeyValueStoreType() == KeyValueStoreType.STORAGE)
+                processStorageNodeHash(keyInformation.getHash());
+        }
     }
 
-    private void processHash(Hash hash) throws IOException {
+    private void processAccountNodeHash(Hash hash) throws IOException {
         Trie trie = this.sourceAccountTrieStore.retrieve(hash);
 
         if (!this.targetAccountTrieStore.exists(hash))
@@ -42,5 +50,27 @@ public class WorldStateCopier {
         for (int k = 0; k < subhashes.length; k++)
             if (subhashes[k] != null)
                 this.hashes.add(new KeyInformation(KeyValueStoreType.ACCOUNTS, subhashes[k]));
+
+        byte[] value = trie.getValue();
+
+        if (value == null)
+            return;
+
+        Account account = AccountEncoder.decode(value);
+
+        this.hashes.add(new KeyInformation(KeyValueStoreType.STORAGE, account.getStorageHash()));
+    }
+
+    private void processStorageNodeHash(Hash hash) throws IOException {
+        Trie trie = this.sourceStorageTrieStore.retrieve(hash);
+
+        if (!this.targetStorageTrieStore.exists(hash))
+            this.targetStorageTrieStore.save(trie);
+
+        Hash[] subhashes = trie.getSubHashes();
+
+        for (int k = 0; k < subhashes.length; k++)
+            if (subhashes[k] != null)
+                this.hashes.add(new KeyInformation(KeyValueStoreType.STORAGE, subhashes[k]));
     }
 }
--- src/test/java/com/ajlopez/blockchain/store/WorldStateCopierTest.java
+++ src/test/java/com/ajlopez/blockchain/store/WorldStateCopierTest.java
@@ -3,10 +3,12 @@ package com.ajlopez.blockchain.store;
 import com.ajlopez.blockchain.core.Account;
 import com.ajlopez.blockchain.core.types.Address;
 import com.ajlopez.blockchain.core.types.Coin;
+import com.ajlopez.blockchain.core.types.DataWord;
 import com.ajlopez.blockchain.core.types.Hash;
 import com.ajlopez.blockchain.encoding.AccountEncoder;
 import com.ajlopez.blockchain.state.Trie;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
+import com.ajlopez.blockchain.vms.eth.TrieStorage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -99,5 +101,84 @@ public class WorldStateCopierTest {
 
             Assert.assertArrayEquals(AccountEncoder.encode(account), AccountEncoder.encode(result));
         }
+    }
+
+    @Test
+    public void copyOneHundredAccountsAndOneHundredStorageCells() throws IOException {
+        Random random = new Random();
+
+        KeyValueStores sourceKeyValueStores = new MemoryKeyValueStores();
+        Stores sourceStores = new Stores(sourceKeyValueStores);
+        AccountStore accountStore = sourceStores.getAccountStoreProvider().retrieve(Trie.EMPTY_TRIE_HASH);
+        TrieStorage trieStorage = sourceStores.getTrieStorageProvider().retrieve(Trie.EMPTY_TRIE_HASH);
+
+        List<Address> addresses = new ArrayList<>();
+        List<Account> accounts = new ArrayList<>();
+        List<DataWord> keys = new ArrayList<>();
+        List<DataWord> values = new ArrayList<>();
+
+        for (int k = 0; k < 100; k++) {
+            Address address = FactoryHelper.createRandomAddress();
+            Account account = new Account(Coin.fromUnsignedLong(random.nextInt(1000)), random.nextInt(1000), 0, null, null);
+
+            addresses.add(address);
+            accounts.add(account);
+
+            accountStore.putAccount(address, account);
+
+            DataWord key = FactoryHelper.createRandomDataWord();
+            DataWord value = FactoryHelper.createRandomDataWord();
+
+            keys.add(key);
+            values.add(value);
+
+            trieStorage.setValue(key, value);
+        }
+
+        trieStorage.commit();
+
+        Address address0 = addresses.get(0);
+        Account account0 = accounts.get(0);
+
+        account0 = new Account(account0.getBalance(), account0.getNonce(), 0, null, trieStorage.getRootHash());
+
+        accounts.set(0, account0);
+
+        accountStore.putAccount(address0, account0);
+
+        accountStore.save();
+
+        Hash rootHash = accountStore.getRootHash();
+
+        KeyValueStores targetKeyValueStores = new MemoryKeyValueStores();
+        Stores targetStores = new Stores(targetKeyValueStores);
+
+        WorldStateCopier worldStateCopier = new WorldStateCopier(sourceStores, targetStores, rootHash);
+
+        worldStateCopier.process();
+
+        TrieStore targetAccountTrieStore = targetStores.getAccountTrieStore();
+        Assert.assertTrue(targetAccountTrieStore.exists(rootHash));
+
+        AccountStore targetAccountStore = targetStores.getAccountStoreProvider().retrieve(rootHash);
+
+        for (int k = 0; k < 100; k++) {
+            Address address = addresses.get(k);
+            Account account = accounts.get(k);
+
+            Account result = targetAccountStore.getAccount(address);
+
+            Assert.assertNotNull(result);
+
+            Assert.assertArrayEquals(AccountEncoder.encode(account), AccountEncoder.encode(result));
+        }
+
+        TrieStore targetStorageTrieStore = targetStores.getStorageTrieStore();
+        Assert.assertTrue(targetStorageTrieStore.exists(account0.getStorageHash()));
+
+        TrieStorage targetTrieStorage = targetStores.getTrieStorageProvider().retrieve(account0.getStorageHash());
+
+        for (int k = 0; k < 100; k++)
+            Assert.assertEquals(values.get(k), targetTrieStorage.getValue(keys.get(k)));
     }
 }
