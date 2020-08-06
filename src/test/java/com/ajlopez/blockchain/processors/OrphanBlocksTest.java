8e8f6947d2754fc042a6df319cb47083194c99d1
ajlopez <webmaster@ajlopez.com>
2020-08-06 19:25:54 -0300

    Additional OrphanBlocks tests

--- src/test/java/com/ajlopez/blockchain/processors/OrphanBlocksTest.java
+++ src/test/java/com/ajlopez/blockchain/processors/OrphanBlocksTest.java
@@ -9,10 +9,50 @@ import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by ajlopez on 03/08/2020.
  */
 public class OrphanBlocksTest {
+    @Test
+    public void addOrphan() {
+        Block genesis = new Block(0, null, null, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block1 = new Block(1, genesis.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+
+        OrphanBlocks orphanBlocks = new OrphanBlocks();
+
+        orphanBlocks.addToOrphans(block1);
+
+        Assert.assertTrue(orphanBlocks.isKnownOrphan(block1.getHash()));
+    }
+
+    @Test
+    public void addChildrenOrphans() {
+        Block genesis = new Block(0, null, null, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block1 = new Block(1, genesis.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block2 = new Block(2, block1.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block2b = new Block(2, block1.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.TWO);
+        Block block3 = new Block(3, block2.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block4 = new Block(4, block3.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block5 = new Block(5, block4.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+
+        OrphanBlocks orphanBlocks = new OrphanBlocks();
+
+        orphanBlocks.addToOrphans(block1);
+        orphanBlocks.addToOrphans(block2);
+        orphanBlocks.addToOrphans(block3);
+        orphanBlocks.addToOrphans(block2b);
+        orphanBlocks.addToOrphans(block5);
+
+        List<Block> result = orphanBlocks.getChildrenOrphanBlocks(block1);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.size());
+        Assert.assertTrue(result.contains(block2));
+        Assert.assertTrue(result.contains(block2b));
+    }
+
     @Test
     public void removeUnknownOrphan() {
         Block genesis = new Block(0, null, null, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
@@ -21,6 +61,22 @@ public class OrphanBlocksTest {
         OrphanBlocks orphanBlocks = new OrphanBlocks();
 
         Assert.assertFalse(orphanBlocks.isKnownOrphan(block1.getHash()));
+
+        orphanBlocks.removeOrphan(block1);
+
+        Assert.assertFalse(orphanBlocks.isKnownOrphan(block1.getHash()));
+    }
+
+    @Test
+    public void removeKnownOrphan() {
+        Block genesis = new Block(0, null, null, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+        Block block1 = new Block(1, genesis.getHash(), MerkleTree.EMPTY_MERKLE_TREE_HASH, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, Address.ZERO, Difficulty.ONE);
+
+        OrphanBlocks orphanBlocks = new OrphanBlocks();
+
+        orphanBlocks.addToOrphans(block1);
+
+        Assert.assertTrue(orphanBlocks.isKnownOrphan(block1.getHash()));
 
         orphanBlocks.removeOrphan(block1);
 
