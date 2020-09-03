d1365320b3f38a7c329002e7de745d12ec2c63f8
ajlopez <webmaster@ajlopez.com>
2020-09-03 10:23:44 -0300

    Fix SELFBALANCE hexadecimal value

--- src/main/java/com/ajlopez/blockchain/vms/eth/OpCodes.java
+++ src/main/java/com/ajlopez/blockchain/vms/eth/OpCodes.java
@@ -61,7 +61,7 @@ public class OpCodes {
     public static final byte DIFFICULTY = 0x44;
     public static final byte GASLIMIT = 0x45;
 
-    public static final byte SELFBALANCE = 0x46;
+    public static final byte SELFBALANCE = 0x47;
 
     public static final byte POP = 0x50;
     public static final byte MLOAD = 0x51;
