2143f178d2cbc0470c2478a0ffedcd7325c852a8
ajlopez <webmaster@ajlopez.com>
2021-01-18 07:39:38 -0300

    BlockStore retrieve block

--- src/test/java/com/ajlopez/blockchain/bc/BlockStoreTest.java
+++ src/test/java/com/ajlopez/blockchain/bc/BlockStoreTest.java
@@ -1,6 +1,10 @@
 package com.ajlopez.blockchain.bc;
 
+import com.ajlopez.blockchain.core.Block;
 import com.ajlopez.blockchain.core.types.BlockHash;
+import com.ajlopez.blockchain.core.types.Difficulty;
+import com.ajlopez.blockchain.encoding.BlockEncoder;
+import com.ajlopez.blockchain.state.Trie;
 import com.ajlopez.blockchain.store.HashMapStore;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
 import org.junit.Assert;
@@ -15,5 +19,19 @@ public class BlockStoreTest {
         BlockHash hash = FactoryHelper.createRandomBlockHash();
 
         Assert.assertNull(blockStore.getBlock(hash));
+    }
+
+    @Test
+    public void retrieveBlockAsNull() throws IOException {
+        Block block = new Block(1, FactoryHelper.createRandomBlockHash(), null, Trie.EMPTY_TRIE_HASH, System.currentTimeMillis() / 1000, FactoryHelper.createRandomAddress(), Difficulty.ONE, 0, 0, null, 0);
+        BlockStore blockStore = new BlockStore(new HashMapStore());
+        BlockHash hash = block.getHash();
+
+        blockStore.saveBlock(block);
+
+        Block result = blockStore.getBlock(hash);
+
+        Assert.assertNotNull(result);
+        Assert.assertArrayEquals(BlockEncoder.encode(block), BlockEncoder.encode(result));
     }
 }
