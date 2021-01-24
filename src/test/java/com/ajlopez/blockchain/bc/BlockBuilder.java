ce3abc6c92f2a134b1b18b3996040942dae5e88a
ajlopez <webmaster@ajlopez.com>
2021-01-24 12:01:32 -0300

    BlockBuilder build block with uncles

--- src/test/java/com/ajlopez/blockchain/bc/BlockBuilder.java
+++ src/test/java/com/ajlopez/blockchain/bc/BlockBuilder.java
@@ -1,13 +1,17 @@
 package com.ajlopez.blockchain.bc;
 
 import com.ajlopez.blockchain.core.Block;
+import com.ajlopez.blockchain.core.BlockHeader;
 import com.ajlopez.blockchain.test.utils.FactoryHelper;
+
+import java.util.List;
 
 /**
  * Created by ajlopez on 23/01/2021.
  */
 public class BlockBuilder {
     private long number;
+    private List<BlockHeader> uncles;
 
     public BlockBuilder number(long number) {
         this.number = number;
@@ -15,7 +19,13 @@ public class BlockBuilder {
         return this;
     }
 
+    public BlockBuilder uncles(List<BlockHeader> uncles) {
+        this.uncles = uncles;
+
+        return this;
+    }
+
     public Block build() {
-        return new Block(FactoryHelper.createBlockHeader(this.number), null, null);
+        return new Block(FactoryHelper.createBlockHeader(this.number), this.uncles, null);
     }
 }
--- src/test/java/com/ajlopez/blockchain/bc/BlockBuilderTest.java
+++ src/test/java/com/ajlopez/blockchain/bc/BlockBuilderTest.java
@@ -1,8 +1,12 @@
 package com.ajlopez.blockchain.bc;
 
 import com.ajlopez.blockchain.core.Block;
+import com.ajlopez.blockchain.core.BlockHeader;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ajlopez on 23/01/2021.
@@ -16,5 +20,28 @@ public class BlockBuilderTest {
 
         Assert.assertNotNull(result);
         Assert.assertEquals(42, result.getNumber());
+    }
+
+    @Test
+    public void createBlockWithUncles() {
+        List<BlockHeader> uncles = new ArrayList<>();
+
+        BlockHeader uncle1a = new BlockBuilder().number(1).build().getHeader();
+        BlockHeader uncle1b = new BlockBuilder().number(1).build().getHeader();
+
+        uncles.add(uncle1a);
+        uncles.add(uncle1b);
+
+        BlockBuilder blockBuilder = new BlockBuilder();
+
+        Block result = blockBuilder.number(2).uncles(uncles).build();
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.getNumber());
+
+        Assert.assertNotNull(result.getUncles());
+        Assert.assertEquals(2, result.getUncles().size());
+        Assert.assertTrue(result.getUncles().contains(uncle1a));
+        Assert.assertTrue(result.getUncles().contains(uncle1b));
     }
 }
