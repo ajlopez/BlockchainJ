1b448747b763b1916d94433c3141c5320986f10d
ajlopez <webmaster@ajlopez.com>
2021-02-07 12:12:46 -0300

    BlockBuilder refactor

--- src/test/java/com/ajlopez/blockchain/bc/BlockBuilder.java
+++ src/test/java/com/ajlopez/blockchain/bc/BlockBuilder.java
@@ -52,13 +52,27 @@ public class BlockBuilder {
     }
 
     public Block build() {
-        if (this.parentHash != null)
-            return new Block(createBlockHeader(this.parentHash, this.number, this.transactions, this.uncles), this.uncles, this.transactions);
+        return new Block(this.buildHeader(), this.uncles, this.transactions);
+    }
 
-        if (this.parent != null)
-            return new Block(createBlockHeader(this.parent, this.transactions, this.uncles), this.uncles, this.transactions);
+    public BlockHeader buildHeader() {
+        BlockHash parentHashToUse;
+        long numberToUse;
 
-        return new Block(createBlockHeader(this.number, this.transactions, this.uncles), this.uncles, this.transactions);
+        if (this.parent != null) {
+            parentHashToUse = this.parent.getHash();
+            numberToUse = this.parent.getNumber() + 1;
+        }
+        else {
+            numberToUse = this.number;
+
+            if (this.parentHash == null)
+                parentHashToUse = numberToUse == 0 ? BlockHash.EMPTY_BLOCK_HASH : FactoryHelper.createRandomBlockHash();
+            else
+                parentHashToUse = this.parentHash;
+        }
+
+        return createBlockHeader(parentHashToUse, numberToUse, this.transactions, this.uncles);
     }
 
     public static BlockHeader createBlockHeader(BlockHash parentHash, long number, List<Transaction> transactions, List<BlockHeader> uncles) {
@@ -73,33 +87,5 @@ public class BlockBuilder {
         Difficulty difficulty = Difficulty.fromUnsignedLong(42);
 
         return new BlockHeader(number, parentHash, ntransactions, transactionsHash, null, nuncles, unclesHash, stateRootHash, System.currentTimeMillis() / 1000, coinbase, difficulty, 12_000_000L, 0, null, 0, 0);
-    }
-
-    public static BlockHeader createBlockHeader(Block parent, List<Transaction> transactions, List<BlockHeader> uncles) {
-        Hash transactionsHash = Block.calculateTransactionsRootHash(transactions);
-        Hash unclesHash = Block.calculateUnclesRootHash(uncles);
-        int ntransactions = transactions == null ? 0 : transactions.size();
-        int nuncles = uncles == null ? 0 : uncles.size();
-
-        Hash stateRootHash = FactoryHelper.createRandomHash();
-        Address coinbase = FactoryHelper.createRandomAddress();
-        Difficulty difficulty = Difficulty.fromUnsignedLong(42);
-
-        return new BlockHeader(parent.getNumber() + 1, parent.getHash(), ntransactions, transactionsHash, null, nuncles, unclesHash, stateRootHash, System.currentTimeMillis() / 1000, coinbase, difficulty, 12_000_000L, 0, null, 0, 0);
-    }
-
-    public static BlockHeader createBlockHeader(long blockNumber, List<Transaction> transactions, List<BlockHeader> uncles) {
-        BlockHash hash = blockNumber == 0 ? BlockHash.EMPTY_BLOCK_HASH : FactoryHelper.createRandomBlockHash();
-        int ntransactions = transactions == null ? 0 : transactions.size();
-        int nuncles = uncles == null ? 0 : uncles.size();
-
-        Hash transactionsHash = Block.calculateTransactionsRootHash(transactions);
-        Hash unclesHash = Block.calculateUnclesRootHash(uncles);
-
-        Hash stateRootHash = FactoryHelper.createRandomHash();
-        Address coinbase = FactoryHelper.createRandomAddress();
-        Difficulty difficulty = Difficulty.fromUnsignedLong(42);
-
-        return new BlockHeader(blockNumber, hash, ntransactions, transactionsHash, null, nuncles, unclesHash, stateRootHash, System.currentTimeMillis() / 1000, coinbase, difficulty, 12_000_000L, 0, null, 0, 0);
     }
 }
