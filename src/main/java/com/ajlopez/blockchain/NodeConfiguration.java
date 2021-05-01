97867833425d2929032a80202b6381f54ea47a89
ajlopez <webmaster@ajlopez.com>
2021-05-01 10:43:16 -0300

    NodeConfiguration WIP

---
+++ src/main/java/com/ajlopez/blockchain/NodeConfiguration.java
@@ -0,0 +1,25 @@
+package com.ajlopez.blockchain;
+
+import javax.xml.soap.Node;
+import java.util.List;
+
+/**
+ * Created by ajlopez on 01/05/2021.
+ */
+public class NodeConfiguration {
+    private final boolean miner;
+    private final int port;
+    private final List<String> hosts;
+
+    public NodeConfiguration(boolean miner, int port, List<String> hosts) {
+        this.miner = miner;
+        this.port = port;
+        this.hosts = hosts;
+    }
+
+    public boolean isMiner() { return this.miner; }
+
+    public int getPort() { return this.port; }
+
+    public List<String> getHosts() { return this.hosts; }
+}
